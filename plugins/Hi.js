f90a614c55cb33a2425e669d94f0fb3ae351e639
kap320 <77118749+kap320@users.noreply.github.com>
2022-04-21 20:18:53 +0700

    Update Hi.js

--- plugins/Hi.js
+++ plugins/Hi.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'KRIZYN BOTz')).buffer(), `
 📮 𝘊𝘈𝘙𝘐 𝘈𝘑𝘈 𝘋𝘐 𝘎𝘐𝘛𝘏𝘜𝘉 𝘖𝘙 𝘠𝘖𝘜 𝘛𝘜𝘉𝘌
 ➵͜͡✪ 𝙶𝙸𝚃𝙷𝚄𝙱 :
-github.com/frostkri/krizynv1
+https://github.com/kap320/krizynv1
 ➵͜͡✪ 𝚈𝙾𝚄 𝚃𝚄𝙱𝙴 :
-youtube.com/c/hokenbeusz
+Bit.ly/3qCgkKv
 ➵͜͡✪ 𝙹𝚊𝚗𝚐𝚊𝚗 𝚕𝚞𝚙𝚊 𝚖𝚊𝚖𝚙𝚒𝚛
-https://lynk.id/kri.com
+https://Trakteer.co/NakanoBot
 `.trim(), ' 𝘋𝘰𝘯𝘵 𝘧𝘰𝘳𝘨𝘦𝘵 𝘵𝘰 𝘧𝘰𝘭𝘭𝘰𝘸 𝘮𝘺 𝘢𝘤𝘤𝘰𝘶𝘯𝘵', 'Thanks', '.tqto', 'Menu','.menu')
 handler.help = ['sourcecode']
 -
